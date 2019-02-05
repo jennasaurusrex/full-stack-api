31b61ac28190cdde7a4c7bf9c79e09de63847f90
JennasaurusRex <jlnorthcott@gmail.com>
2019-02-05 16:18:07 -0500

    Add editable function in recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,7 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :ingredients, :instructions, :user_id, :user
+  attributes :id, :name, :description, :ingredients, :instructions, :user_id, :user, :editable
+
+  def editable
+    scope == object.user
+  end
 end
